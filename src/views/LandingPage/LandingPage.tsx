c21f19823ef8dc9ee264e06232d36b07b85f6af1
Casperfeng <casp.feng@gmail.com>
2019-11-02 18:29:49 +0100

    add mock data to courselist

--- src/views/LandingPage/LandingPage.tsx
+++ src/views/LandingPage/LandingPage.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import Courses from '../../components/Courses/Courses';
+
+const PageContent = styled.div`
+  margin: auto;
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-self: center;
+  justify-content: center;
+`;
+
+const CoursesContainer = styled.div``;
 
 export default function LandingPage() {
   return (
-    <div>
-      <Courses />
-    </div>
+    <PageContent>
+      <CoursesContainer>
+        <Courses />
+      </CoursesContainer>
+    </PageContent>
   );
 }
