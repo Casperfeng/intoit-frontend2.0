694248efa0dff3cfaa14bda0031a4103e539f0dc
Casperfeng <casp.feng@gmail.com>
2019-11-02 20:54:22 +0100

    add cloud overlay to background

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import LandingPage from './views/LandingPage/LandingPage';
 import Navbar from './components/Navbar/Navbar';
 
+const Main = styled.div`
+  min-height: 1000px;
+`;
+
 export default function App() {
   return (
-    <>
+    <Main>
       <Navbar />
       <LandingPage />
-    </>
+    </Main>
   );
 }
