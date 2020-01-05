5889fcb154c1015996ec9d6876a1776705ddd6e3
Casperfeng <casp.feng@gmail.com>
2020-01-05 15:37:26 +0100

    change format of Title/SubTitle components

--- src/components/Title/SubTitle.tsx
+++ src/components/Title/SubTitle.tsx
@@ -9,6 +9,10 @@ export default function Title({ children }: Props) {
   const StyledTitle = styled.h1`
     font-size: 32px;
     font-weight: bold;
+    text-transform: lowercase;
+    ::first-letter {
+      text-transform: capitalize;
+    }
   `;
   return <StyledTitle>{children}</StyledTitle>;
 }
