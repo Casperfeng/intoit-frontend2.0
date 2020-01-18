3c84d80442ee2178398267cd8bedbee771b9f571
eirsture <eirik.sture@gmail.com>
2020-01-18 15:47:42 +0100

    Added Eirik as team member

--- src/views/AboutUs/components/TeamMembers/TeamMembers.tsx
+++ src/views/AboutUs/components/TeamMembers/TeamMembers.tsx
@@ -20,7 +20,8 @@ export default function TeamMembers() {
     { name: 'Casper Feng', linkedIn: 'casperfeng' },
     { name: 'Andreas Rikheim', linkedIn: 'andreas-rikheim' },
     { name: 'Håkon Størdal', linkedIn: 'håkons' },
-    { name: 'Gulleik Olsen', linkedIn: 'gulleik-olsen' }
+    { name: 'Gulleik Olsen', linkedIn: 'gulleik-olsen' },
+    { name: 'Eirik Sture', linkedIn: 'eirik-sture'}
   ];
 
   const teamMembers = teamMembersInfo.map((teamMember, index) => (
