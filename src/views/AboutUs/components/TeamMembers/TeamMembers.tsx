cda5a68af343868d58233dfe56a032115a258cd5
eirsture <37185897+eirsture@users.noreply.github.com>
2020-01-20 23:08:21 +0100

    Removed old team member
    Removed Gulleik as team member

--- src/views/AboutUs/components/TeamMembers/TeamMembers.tsx
+++ src/views/AboutUs/components/TeamMembers/TeamMembers.tsx
@@ -20,7 +20,6 @@ export default function TeamMembers() {
     { name: 'Casper Feng', linkedIn: 'casperfeng' },
     { name: 'Andreas Rikheim', linkedIn: 'andreas-rikheim' },
     { name: 'Håkon Størdal', linkedIn: 'håkons' },
-    { name: 'Gulleik Olsen', linkedIn: 'gulleik-olsen' },
     { name: 'Eirik Sture', linkedIn: 'eirik-sture'}
   ];
 
