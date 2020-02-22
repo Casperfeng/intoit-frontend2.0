3cbd1a055af97f965d71f6edf97289a55bd50e7c
Alex Nguyen <alex.nguyen@fortedigital.no>
2020-02-22 16:38:07 +0100

    Refactor coursecard, styledLink and other stylecomps as separate components

--- src/views/Home/components/CourseCard.tsx
+++ src/views/Home/components/CourseCard.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import styled from 'styled-components/macro';
-import { Card } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import CourseCardWrapper, { CourseIllustration, CourseName, SchoolCode, StyledAccessTime, StyledFavorite, CourseStatus } from './CourseCardStyles';
+import StyledLink from 'components/StyledLink/StyledLink';
 import { AccessTime, Favorite } from 'styled-icons/material';
 import { courseCardDeterminator } from 'shared/util';
-
-import colors from 'shared/colors';
 
 interface CourseProps {
   id: number;
@@ -48,13 +45,11 @@ const CourseCard = ({
   const icon = courseCardInfo.icon;
   return (
     <StyledLink to={`/courses/${id}`}>
-      <Wrapper color={color}>
-        <Illustration src={icon} alt="Course icon" />
-        <div>
-          <Name>{name}</Name>
-          <SchoolCode>{code}</SchoolCode>
-        </div>
-        <Status>
+      <CourseCardWrapper color={color}>
+        <CourseIllustration src={icon} alt="Course icon" />
+        <CourseName>{name}</CourseName>
+        <SchoolCode>{code}</SchoolCode>
+        <CourseStatus>
           <StyledAccessTime>
             <AccessTime size={22} />
             {/* Remove hardcode date. Implement new date system with date-fns */}
@@ -64,89 +59,10 @@ const CourseCard = ({
             <Favorite size={22} />
             <p>{numFavoritesAllTime}</p>
           </StyledFavorite>
-        </Status>
-      </Wrapper>
+        </CourseStatus>
+      </CourseCardWrapper>
     </StyledLink>
   );
 };
 
-const StyledLink = styled(Link)`
-  text-decoration: none;
-
-  &:focus,
-  &:hover,
-  &:visited,
-  &:link,
-  &:active {
-    text-decoration: none;
-  }
-`;
-
-const Illustration = styled.img`
-  max-width: 70%;
-  max-height: 125px;
-  object-fit: contain;
-  margin: auto;
-`;
-
-const Wrapper = styled(Card)`
-  display: flex;
-  flex-direction: column;
-  min-height: 270px;
-  border: 0;
-  justify-content: space-between;
-  padding: 16px;
-  ${(props: { color: string }) => `
-    border: 2px solid ${props.color};
-  `}
-
-  transition: all 0.2s ease-in-out;
-
-  &:hover {
-    transform: scale(1.025) translateY(-2px);
-  }
-`;
-
-const Name = styled.p`
-  font-weight: 500;
-  font-size: 20px;
-  text-decoration: capitalize;
-  margin-bottom: 7px;
-`;
-
-const SchoolCode = styled.p`
-  font-size: 12px;
-  text-decoration: uppercase;
-`;
-
-const StyledAccessTime = styled.div`
-  color: ${colors.grey};
-  display: flex;
-  align-items: center;
-
-  p {
-    margin-left: 5px;
-    margin-top: 3px;
-  }
-`;
-
-const StyledFavorite = styled.div`
-  display: flex;
-  align-items: center;
-
-  p {
-    margin-left: 2px;
-    margin-top: 2px;
-    font-weight: 500;
-  }
-
-  ${(props: { color: string }) => `
-    color: ${props.color};
-  `}
-`;
-const Status = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 20px;
-`;
 export default CourseCard;
