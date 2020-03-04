c25a7ce1e5a39f49ce9fbe5570033fddb1d32750
Alex Nguyen <alex.nguyen@fortedigital.no>
2020-03-04 22:19:23 +0100

    Comment constants.ts

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -3,6 +3,13 @@ import 'moment/locale/nb';
 import moment from 'moment';
 moment.locale('nb');
 
+// * Uncomment these and comment out the corresponding variables under in order to test with production database
+// ! We should not overabuse this since we generate incorrect data
+/* 
+const BASE_URL = 'https://ace-restapi.herokuapp.com';
+const FACEBOOK_APP_ID = '274754369699301'; 
+*/
+
 const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://ace-restapi.herokuapp.com' : 'http://intoit-dev.herokuapp.com';
 
 const FACEBOOK_APP_ID = process.env.NODE_ENV === 'production' ? '274754369699301' : '389917801835190';
