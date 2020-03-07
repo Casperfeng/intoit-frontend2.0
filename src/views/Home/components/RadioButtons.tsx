4a681b34bcb361576efc098f2ec11a86b5e40757
eirsture <eirik.sture@gmail.com>
2020-03-07 19:54:09 +0100

    makes sure one button is always shown as active

--- src/views/Home/components/RadioButtons.tsx
+++ src/views/Home/components/RadioButtons.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import styled from 'styled-components/macro';
+import styled, { css } from 'styled-components/macro';
 import Chip from '@material-ui/core/Chip';
+import { buttonColors } from 'shared/colors';
 import { fetchSchools } from 'redux/duck/schoolDuck';
 import { fetchCourses } from 'redux/duck/coursesDuck';
 
 export default function RadioButtons() {
+  const [selected, setSelected] = useState(0);
   const schools = useSelector((state: ReduxState) => state.schools);
   const dispatch = useDispatch();
 
@@ -14,16 +16,37 @@ export default function RadioButtons() {
     // eslint-disable-next-line
   }, []);
 
-  const schoolButtons = schools.map(school => <Chip key={school.id} label={school.name} onClick={() => handleClick(school.id)} />);
-
   const handleClick = id => {
+    setSelected(id);
     dispatch(fetchCourses('', id));
   };
-  console.log('schools', schools);
+
+  const schoolButtons = schools.map(school => (
+    <RadioButton key={school.id} label={school.name} onClick={() => handleClick(school.id)} selected={selected === school.id ? true : false} />
+  ));
+
+  console.log('Selected: ', selected);
+
   return (
     <div>
-      <Chip label={'Alle'} onClick={() => handleClick(0)} />
+      <RadioButton label={'Alle'} onClick={() => handleClick(0)} selected={selected === 0 ? true : false} />
       {schoolButtons}
     </div>
   );
 }
+const RadioButton = styled(Chip)`
+  margin: 2px;
+  margin-bottom: 30px;
+  color: white;
+  background: ${buttonColors.default};
+  &:focus,
+  &:hover,
+  &:active {
+    background: ${buttonColors.clicked};
+  }
+  ${props =>
+    props.selected &&
+    css`
+      background: ${buttonColors.clicked};
+    `}
+`;
