f479e46663c068faeed54661a9f94f92d55136fa
Alex Nguyen <hoanghai92@hotmail.com>
2020-03-20 19:08:04 +0100

    Fix symbolDeterminator

--- src/views/LastUpdates/LastUpdates.tsx
+++ src/views/LastUpdates/LastUpdates.tsx
@@ -13,7 +13,7 @@ import { Education } from '@styled-icons/zondicons/Education';
 import { LightBulb } from '@styled-icons/heroicons-outline/LightBulb';
 import { CommentAdd } from '@styled-icons/boxicons-solid/CommentAdd';
 import { Plus } from '@styled-icons/boxicons-regular/Plus';
-import DEFAULT_ICON from 'assets/icons/onlineTestIcon.svg';
+import { QuestionMark } from '@styled-icons/boxicons-regular/QuestionMark';
 
 interface RouterParams {
   id: string;
@@ -31,17 +31,13 @@ export default function LastUpdate(props: UpdateProps) {
   const courseInfo = useSelector((state: ReduxState) => state.courseInfo);
   const feed = useSelector((state: ReduxState) => state.resource.feed);
   const dispatch = useDispatch();
-  const feedSymbol = symbolDeterminator(feed.symbol);
-  const icon = feedSymbol.icon;
 
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     async function retrieveUpdate() {
-      console.log('efe');
       await dispatch(fetchFeeds(id));
       await setLoading(false);
-      console.log(feed);
     }
     retrieveUpdate();
   }, []);
@@ -57,12 +53,16 @@ export default function LastUpdate(props: UpdateProps) {
 
       {/* //* Andreas: Her prøver du å vise en objects ved å wrappe den inni i et div. Det vil ikke fungere. En normal approach her er å bruke .map() */}
 
-      {feed.map((feed: any, i: any) => (
-        <FeedUpdate key={i}>
-          <FeedSymbol>{icon}</FeedSymbol>
-          <FeedMessage>{feed.message}</FeedMessage>
-        </FeedUpdate>
-      ))}
+      {feed.map((element: any, i: number) => {
+        console.log('element :', element.symbol);
+
+        return (
+          <FeedUpdate key={i}>
+            <FeedSymbol>{symbolDeterminator(element.symbol)}</FeedSymbol>
+            <FeedMessage>{element.message}</FeedMessage>
+          </FeedUpdate>
+        );
+      })}
     </ContentLayout>
   );
 }
@@ -84,21 +84,23 @@ const FeedMessage = styled.div`
 `;
 
 function symbolDeterminator(feedSymbol: string) {
+  const iconSize = 24;
+
   if (feedSymbol.toUpperCase().includes('UP')) {
-    return { icon: Like };
+    return <Like size={iconSize} />;
   } else if (feedSymbol.toUpperCase().match('DOWN')) {
-    return { icon: Dislike };
+    return <Dislike size={iconSize} />;
   } else if (feedSymbol.toUpperCase().match('PENCIL')) {
-    return { icon: EditOutline };
+    return <EditOutline size={iconSize} />;
   } else if (feedSymbol.toUpperCase().match('PLUS')) {
-    return { icon: Education };
+    return <Education size={iconSize} />;
   } else if (feedSymbol.toUpperCase().match('HINT')) {
-    return { icon: LightBulb };
+    return <LightBulb size={iconSize} />;
   } else if (feedSymbol.toUpperCase().match('COMMENT')) {
-    return { icon: CommentAdd };
+    return <CommentAdd size={iconSize} />;
   } else if (feedSymbol.toUpperCase().match('PLUS')) {
-    return { icon: Plus };
+    return <Plus size={iconSize} />;
   } else {
-    return { icon: DEFAULT_ICON };
+    return <QuestionMark size={iconSize} />;
   }
 }
