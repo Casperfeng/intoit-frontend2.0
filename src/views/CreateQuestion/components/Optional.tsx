d9f5c2f9ce350cd3c1467d98d828c0d131df2a46
kmidtlid3@gmail.com <kmidtlid3@gmail.com>
2020-03-22 19:36:58 +0100

    Save field value in state

--- src/views/CreateQuestion/components/Optional.tsx
+++ src/views/CreateQuestion/components/Optional.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import { Grid, Typography, TextField } from '@material-ui/core';
 
-export default function Optional(props) {
+export default function Optional() {
+  const [hintValue, setHintValue] = useState('');
+  const [explanationValue, setExplanationValue] = useState('');
+
+  const handleHintChange = event => {
+    setHintValue(event.target.value);
+  };
+
+  const handleExplanationChange = event => {
+    setExplanationValue(event.target.value);
+  };
+
   return (
     <Grid container direction="column" spacing={1}>
       <Grid item xs>
@@ -12,12 +23,12 @@ export default function Optional(props) {
       </Grid>
       <Grid item xs>
         <FormControl fullWidth>
-          <TextField multiline placeholder="Hint (valgfritt)" variant="outlined" />
+          <TextField multiline value={hintValue} placeholder="Hint (valgfritt)" variant="outlined" onChange={handleHintChange} />
         </FormControl>
       </Grid>
       <Grid item xs>
         <FormControl fullWidth>
-          <TextField multiline placeholder="Forklaring (valgfritt)" variant="outlined" />
+          <TextField multiline value={explanationValue} placeholder="Forklaring (valgfritt)" variant="outlined" onChange={handleExplanationChange} />
         </FormControl>
       </Grid>
     </Grid>
--- src/views/CreateQuestion/components/QuestionInput.tsx
+++ src/views/CreateQuestion/components/QuestionInput.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import { Grid, Typography, TextField } from '@material-ui/core';
 
 export default function QuestionInput({ title }) {
+  const [value, setValue] = useState('');
+
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
+
   return (
     <Grid container spacing={1}>
       <Grid item xs={12}>
@@ -12,7 +18,7 @@ export default function QuestionInput({ title }) {
       </Grid>
       <Grid item xs={12}>
         <FormControl fullWidth required={true}>
-          <TextField multiline rows="4" placeholder={`Ditt ${title}`} variant="outlined" />
+          <TextField multiline rows="4" value={value} placeholder={`Ditt ${title}`} variant="outlined" onChange={handleChange} />
         </FormControl>
       </Grid>
     </Grid>
