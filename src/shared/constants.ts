54e24d71306631d2d4914671cade439677e38ea4
Casper Feng <casp.feng@gmail.com>
2020-04-04 20:54:43 +0200

    set up GA tracking for dev and prod

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -19,9 +19,11 @@ export const questionTypes = {
 
 const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://ace-restapi.herokuapp.com' : 'http://intoit-dev.herokuapp.com';
 
+const GOOGLE_ANALYTICS_ID = process.env.NODE_ENV === 'production' ? 'UA-162793965-1' : 'UA-162793965-2';
+
 const FACEBOOK_APP_ID = process.env.NODE_ENV === 'production' ? '274754369699301' : '389917801835190';
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.baseURL = BASE_URL;
 
-export { FACEBOOK_APP_ID };
+export { FACEBOOK_APP_ID, GOOGLE_ANALYTICS_ID };
