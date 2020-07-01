c07b4253ab02fcc30cfaedb3d16693d30389c98a
eirsture <eirik.sture@gmail.com>
2020-07-01 17:24:45 +0200

    remove try/catch because only a comment/user in backend database contains invalid avatar

--- src/components/Discussion/Comment.tsx
+++ src/components/Discussion/Comment.tsx
@@ -41,19 +41,9 @@ export default function Comment({ comment, inQuiz, setReplyTo }: CommentProps) {
   };
 
   const showFacebookPic = () =>
-    {
-      try {
-        if (comment.facebook_id && comment.show_facebook_pic) {
-          return `https://graph.facebook.com/${comment.facebook_id}/picture`
-        } else {
-          return require(`../../assets/badges/${comment.avatar}.png`);
-        }
-      }
-      catch(err) {
-        return require(`../../assets/badges/0.png`)
-      }
-    }
-      
+    comment.facebook_id && comment.show_facebook_pic
+      ? `https://graph.facebook.com/${comment.facebook_id}/picture`
+      : require(`../../assets/badges/${comment.avatar}.png`);
 
   return (
     <CommentWrapper reply={comment.reply_to}>
