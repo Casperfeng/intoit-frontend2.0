488fdcd2acffeb7cb976812d1f63fb3fff15ae2d
Andreas Rikheim <andreasrik.asr@gmail.com>
2020-07-01 11:51:51 +0200

    fix the grey box with xp per activity

--- src/views/Levels/Activities.tsx
+++ src/views/Levels/Activities.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components/macro';
 
 export default function Activities() {
   const dispatch = useDispatch();
-  const activities = (state: ReduxState) => state.resource.activities;
+  const activities = useSelector((state: ReduxState) => state.resource.activities);
 
   useEffect(
     () => {
@@ -17,30 +17,61 @@ export default function Activities() {
   );
   return (
     <LevelUpInfo>
-      <p>{activities.length}</p>
-      <h3>Hvordan går jeg opp i nivå?</h3>
+      <h2>Hvordan går jeg opp i nivå?</h2>
       <Xp>+5xp</Xp>
-      <div>Noen stemmer opp din kommentarer</div>
-      <div>Noen stemmer opp din kommentarer</div>
-      <div>Noen stemmer opp din kommentarer</div>
+      {activities
+        .filter(e => e.reinforcement === 5)
+        .map((element: any, i: number) => {
+          return <ActivitiesStyle>{element.description}</ActivitiesStyle>;
+        })}
       <Xp>+25xp</Xp>
-      <div>Noen stemmer opp din kommentarer</div>
+      {activities
+        .filter(e => e.reinforcement === 25)
+        .map((element: any, i: number) => {
+          return <ActivitiesStyle>{element.description}</ActivitiesStyle>;
+        })}
       <Xp>+50px</Xp>
-      <div>Noen stemmer opp din kommentarer</div>
-      <div>Noen stemmer opp din kommentarer</div>
+      {activities
+        .filter(e => e.reinforcement === 50)
+        .map((element: any, i: number) => {
+          return <ActivitiesStyle>{element.description}</ActivitiesStyle>;
+        })}
       <Xp>+100xp</Xp>
-      <div>Noen stemmer opp din kommentarer</div>
-      <Xp>+125xp</Xp>
-      <div>Noen stemmer opp din kommentarer</div>
+      {activities
+        .filter(e => e.reinforcement === 100)
+        .map((element: any, i: number) => {
+          return <ActivitiesStyle>{element.description}</ActivitiesStyle>;
+        })}
       <Xp>+200xp</Xp>
-      <div>Noen stemmer opp din kommentarer</div>
-      <Xp>+300px</Xp>
-      <div>Noen stemmer opp din kommentarer</div>
-      <div>Noen stemmer opp din kommentarer</div>
+      {activities
+        .filter(e => e.reinforcement === 200)
+        .map((element: any, i: number) => {
+          return <ActivitiesStyle>{element.description}</ActivitiesStyle>;
+        })}
+      <Xp>+300xp</Xp>
+      {activities
+        .filter(e => e.reinforcement === 300)
+        .map((element: any, i: number) => {
+          return <ActivitiesStyle>{element.description}</ActivitiesStyle>;
+        })}
       <Xp>+500xp</Xp>
-      <div>Noen stemmer opp din kommentarer</div>
+      {activities
+        .filter(e => e.reinforcement === 500)
+        .map((element: any, i: number) => {
+          return <ActivitiesStyle>{element.description}</ActivitiesStyle>;
+        })}
+      <Xp>+500xp</Xp>
+      {activities
+        .filter(e => e.reinforcement === 500)
+        .map((element: any, i: number) => {
+          return <ActivitiesStyle>{element.description}</ActivitiesStyle>;
+        })}
       <Xp>+1000xp</Xp>
-      <div>Noen stemmer opp din kommentarer</div>
+      {activities
+        .filter(e => e.reinforcement === 1000)
+        .map((element: any, i: number) => {
+          return <ActivitiesStyle>{element.description}</ActivitiesStyle>;
+        })}
     </LevelUpInfo>
   );
 }
@@ -48,13 +79,21 @@ export default function Activities() {
 const LevelUpInfo = styled.div`
   padding: 20px;
   margin-left: 100px;
-  margin-top: 100px;
+  margin-top: 250px;
   background-color: rgba(196, 196, 196, 0.5);
   color: black;
   width: 40%;
+  height: 60%;
 `;
 
 const Xp = styled.h5`
   margin-top: 15px;
   margin-bottom: 5px;
+  font-size: 20px;
+`;
+
+const ActivitiesStyle = styled.div`
+  margin-top: 10px;
+  margin-left: 3px;
+  font-size: 20px;
 `;
